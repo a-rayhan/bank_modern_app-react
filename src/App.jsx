9ac27958093242392e32ac15d1c514f120a56794
rayhan <aburayhandh@gmail.com>
2023-03-18 10:29:09 +0600

    CardDeal section added

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
           <Stats />
           <Business />
           <Billing />
-          {/* <CardDeal /> */}
+          <CardDeal />
           {/* <Testimonials /> */}
           {/* <Clients /> */}
           {/* <CTA /> */}
