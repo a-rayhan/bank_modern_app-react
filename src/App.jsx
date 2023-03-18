fc9451f4a4c8f606dbe041b91d098da3b3db9a34
rayhan <aburayhandh@gmail.com>
2023-03-18 10:33:09 +0600

    Testimonials section added

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
           <Business />
           <Billing />
           <CardDeal />
-          {/* <Testimonials /> */}
+          <Testimonials />
           {/* <Clients /> */}
           {/* <CTA /> */}
           {/* <Footer /> */}
